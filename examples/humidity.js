37a18216713edeb563ef9404a5695a2aaaf63526
Vitaliy Gribko <vitaliy.gribko@tibbo.com>
2016-06-21 11:30:09 +0300

    Updates for lates LTPS C++ API

--- examples/humidity.js
+++ examples/humidity.js
@@ -1,9 +1,9 @@
 var lib = require("../libs/ltps_humidity");
 
-var data = new lib.Hih6130();
+var data = new lib.HumData();
 var hum = new lib.Humidity();
 
-hum.getData("-s01", data);
+hum.getData("s1", data);
 
 console.log("RH: %d%", data.humidity);
 console.log("Temperature: %d degrees Celsius", data.temperature);
