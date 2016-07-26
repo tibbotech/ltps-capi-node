dedabdc7c10b028b6315fe6e63ce2609f31009a5
Vitaliy Gribko <vitaliy.gribko@tibbo.com>
2016-07-26 14:20:22 +0300

    Update PIC example

--- examples/pic.js
+++ examples/pic.js
@@ -11,16 +11,16 @@ for (var i = 1; i < 4; i++) // Support by Tibbit's #16, #17 and #31
 	
 	sleep.sleep(1);
 	
-	console.log("Start PWM at channel %d (pusle = %d, period = %d, prescaler = %d)", i, 100, i * 300, 1);
+	console.log("Start PWM at channel %d (pusle = %d, period = %d, prescaler = %d)", i, 100 * i, 512, 1);
 	/*!
 		\param socket I2C bus name (eg: s1, s15)
 		\param channel Channel number (1, 2 or 3)
  		\param pulse PWM pulse width is defined as the base frequency clock count (0, 1, 2...1023)
 		\param period PWM period is defined as the base frequency clock count (0, 4, 8...1023)
 		\param prescaler Prescaler affects the base frequency (the period is multiplied to the prescaler) (1, 4, 16 or 64)
-   */
+	*/
    // void startPwm(const char *socket, int channel, int pulse, int period, int prescaler);
-	pic.startPwm("s1", i, 100, i * 300, 1);
+	pic.startPwm("s1", i, 100 * i, 512, 1);
 	sleep.sleep(3);
 	
 	console.log("Stop PWM at channel %d", i);
